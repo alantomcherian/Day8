5ed84a845d0509966c18e01740e47d1e7cbf09f0
alantomcherian <atcherian101@gmail.com>
2021-08-09 20:14:21 +0530

    feat: uc3-part_time

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace Day8_1
     {
         static void Main(string[] args)
         {
+            int IS_PART_TIME = 1;
             int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             
@@ -14,7 +15,12 @@ namespace Day8_1
             Random random = new Random();
             
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_FULL_TIME)
+
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
             }
