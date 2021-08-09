068cf33d27969b2bb9c7bc54e2867af68843c241
alantomcherian <atcherian101@gmail.com>
2021-08-09 20:06:44 +0530

    feat: uc2-employee wage

--- Program.cs
+++ Program.cs
@@ -6,19 +6,24 @@ namespace Day8_1
     {
         static void Main(string[] args)
         {
-            int IS_FULL_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
-
-            int empCheck = random.Next(0, 2);
-
+            
+            int empCheck = random.Next(0, 3);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage: " + empWage);
         }
     }
 }
