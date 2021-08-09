7d4a44701ec5973463de5a9c455ee231b9efe8b4
alantomcherian <atcherian101@gmail.com>
2021-08-09 19:20:19 +0530

    feat:initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+        }
+    }
+}
