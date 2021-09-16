28c253d5c6325b22300e09bf18559516a488ce00
alantomcherian <atcherian101@gmail.com>
2021-09-16 18:56:27 +0530

    feat: UC6-Wage calculation till total working hours or max days

--- Program.cs
+++ Program.cs
@@ -7,15 +7,17 @@ namespace Day8_1
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 2;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_PER_MONTH = 100;
         static void Main(string[] args)
         {
             int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
+            int totalWorkingDays = 0;
+            int totalEmpHrs= 0;
 
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            while(totalEmpHrs <= MAX_HRS_PER_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
 
@@ -31,10 +33,11 @@ namespace Day8_1
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Emp Wage: " + empWage);
+                
+                totalEmpHrs += empHrs;
+                Console.WriteLine("days :"+ totalWorkingDays+" Emp hrs : "+empHrs);
             }
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Emp Wage: " + totalEmpWage);
         }
     }
